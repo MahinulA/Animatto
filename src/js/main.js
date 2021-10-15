28c4d4b7e5013be142e070762d50a5080a9220bf
Mahinul Mostafa Abid <himahinulabid@gmail.com>
2021-10-16 00:12:26 +0600

    initiated fadeIn animation

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+let textFadeIn =(()=>{
+    console.log("hellow");
+    let fadeInClassInit= document.querySelectorAll(".fadeIn");
+    // console.log(fadeInClassInit);
+    for(i =0; i < fadeInClassInit.length; i++){
+        fadeInClass = fadeInClassInit[i];
+        //all the class selected
+    }
+})();
