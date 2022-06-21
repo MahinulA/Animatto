33887b59f7af869a4401c06be6b82a6bbae9474e
MahinulA <himahinulabid@gmail.com>
2022-06-21 23:14:52 +0600

    fadeout animation added

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,8 @@
 // FADEIN ANIMATION
 // MAKE IT INVISIBLE THEN VISIBLE 
 // SET TRANSITION 
-let fadeInSelectorInit =document.querySelectorAll(".fadeIn");
-for(counter =0; counter < fadeInSelectorInit.length; counter++){
+let fadeInSelectorInit =document.querySelectorAll(".animatto-fadeIn");
+for(let counter =0; counter < fadeInSelectorInit.length; counter++){
     let fadeInSelector = fadeInSelectorInit[counter]; //selected all fadein elements
     window.addEventListener("load", ()=>{
         console.log("element loaded");
@@ -20,3 +20,18 @@ for(counter =0; counter < fadeInSelectorInit.length; counter++){
 
     // FIRST SIMPLE ANIMATION UPDATE: SUCCESSFUL
 }
+
+
+// CSS: MAKE IT OPACITY 1
+// IN JAVASCRIPT MAKE MAKE IT OPACITY 0 (DOM MANIPULATIONS)
+// FADEOUT ANIMATIONS
+let fadeOutSelectorInit = document.querySelectorAll(".animatto-fadeOut");
+for(let counter= 0; counter <fadeOutSelectorInit.length; counter++){
+    let fadeOutSelector = fadeOutSelectorInit[counter];
+    fadeOutSelector.style.opacity = "0";
+
+    // FIRST SIMPLE ANIMATION UPDATE: SUCCESSFUL
+}
+
+
+
