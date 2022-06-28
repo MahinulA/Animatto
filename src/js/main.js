81970d6f4add39c3b9b245c14acdac5b94de69ee
MahinulA <himahinulabid@gmail.com>
2022-06-28 21:47:29 +0600

    made some changes

--- src/js/main.js
+++ src/js/main.js
@@ -39,16 +39,12 @@ let AnimattoTimer = (QueryElement) =>{
 
 
 // FADEIN ANIMATION, MAKE IT INVISIBLE THEN VISIBLE, SET TRANSITION  
-let fadeInSelectorInit =document.querySelectorAll(".animatto-fadeIn");
+// let fadeInSelectorInit =document.querySelectorAll(".animatto-fadeIn");
 let FadeIn =(QueryElement) =>{
     for(let counter =0; counter < QueryElement.length; counter++){
         let Selector = QueryElement[counter]; //selected all fadein elements
         window.addEventListener("load", ()=>{
-            // console.log("element loaded");
             Selector.style.opacity ="1";
-            // setTimeout(()=>{
-            //     Selector.style.display = "none";
-            // }, 2000); 
         });
     }
 };
@@ -60,7 +56,6 @@ let fadeOutSelectorInit = document.querySelectorAll(".animatto-fadeOut");
 
 let FadeOut = (QueryElement) =>{
     for(let counter= 0; counter <QueryElement.length; counter++){
-
         let Selector = QueryElement[counter];
         window.addEventListener("load", ()=>{
             //setting the opacity to 0 which was value 1 in "animatto-fadeOut" class
@@ -75,34 +70,44 @@ let FadeOut = (QueryElement) =>{
 
 // SLIDE FADE-IN ANIMATION
 let slideFadeInSelectorInit = document.querySelectorAll(".animatto-slide-fadeIn");
-
-let SlideFadeIn = () =>{
-
+let SlideFadeIn = (QueryElement) =>{
+    for(let counter =0; counter < QueryElement.length; counter++){
+        let Selector = QueryElement[counter];
+        window.addEventListener("load" , ()=>{
+            Selector.style.transform = "translateX(0%)";
+            Selector.style.opacity = "1";
+        });
+    }
 };
-for(let counter =0; counter < slideFadeInSelectorInit.length; counter++){
-    let slideFadeInSelector = slideFadeInSelectorInit[counter];
-    window.addEventListener("load" , ()=>{
-        slideFadeInSelector.style.transform = "translateX(0%)";
-        slideFadeInSelector.style.opacity = "1";
-    });
-}
+
 
 
 // SLIDE FADE-OUT ANIMATION
 let slideFadeOutSelectorInit= document.querySelectorAll(".animatto-slide-fadeOut");
-let oneSecond = 1000;
-for(let counter = 0; counter < slideFadeOutSelectorInit.length; counter++){
-    let slideFadeOutSelector =slideFadeOutSelectorInit[counter];
-    window.addEventListener("load", ()=>{
-        slideFadeOutSelector.style.transform= "translateX(30%)";
-        slideFadeOutSelector.style.opacity = "0" ;
-        setTimeout(()=>{
-            slideFadeOutSelector.style.display= " none";
-        },oneSecond);
-    });
+
+let SlideFadeOut = (QueryElement) =>{
+    for(let counter = 0; counter < QueryElement.length; counter++){
+        let Selector =QueryElement[counter];
+        window.addEventListener("load", ()=>{
+            Selector.style.transform= "translateX(30%)";
+            Selector.style.opacity = "0" ;
+            setTimeout(()=>{
+                Selector.style.display= " none";
+            },1000); //this will be similar to the animation timer
+        });
+    }
+};
+
+
+
+
+
+
+
+
+//running the events
+let fadeInSelectorInit =document.querySelectorAll(".animatto-slide-fadeOut");
+if(fadeInSelectorInit.length > 0){
+    SlideFadeOut(fadeInSelectorInit);
 }
-
-
-
-
 
